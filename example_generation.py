8fae8b7bf38ecf54bbffcef240d2313276687c17
PabloAmedo <60137739+PabloAmedo@users.noreply.github.com>
2023-09-12 11:26:43 +0200

    Bug fixing
    - Fixed transverse diffusion
    - Example files now splitted in two

---
+++ example_generation.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 12 11:16:45 2023
+
+@author: Pablo
+"""
+
+from Alpha_track_simulator import*
+
+
+
+# This is an example of how to generate alpha tracks using this generator
+
+# Create an argon object from the gas class
+argon=Gas(1,1,1,1,1)
+
+# Create a source and a list to store the tracks
+
+source=Source(radius=0.1) #Radius in cm
+
+track_list=[]
+
+# Set some parameters for the alpha track production
+n_tracks=1;ath_angle=0
+
+exposition_time=n_tracks/source.rate #In seconds
+
+# Generate the alpha tracks using the Bragg profile
+source.produce_alpha(n=n_tracks,store=track_list,ath_in=None,ionization_profile="Bragg")
+
+
+#Create a difussion handler and change the diffusion of the tracks (sigma in cm)
+diff_handler=Diffusion_handler(sigma_diff=0.18,sigma_PSF=0)
+# Update the diffusion for each track
+diff_handler.diffuse(track_list)
+
+# Generate the electrons alongside the tracks. This will generate the original positions of
+# the electrons and the diffused positions as well
+for i in track_list: i.fill()
+
+
+#Create a noise object with a given dark noise
+noise=Noise(50)
+
+#Plot the tracks
+image2d=Image_2D(track_list=track_list,hist_args={"bins":100})
+# #Plot the tracks
+image2d.track_plot()
+image2d.plot_hist(noise_object=noise,exposition_time=exposition_time)
+image2d.plot_x()
+
+
+
+
+
+
+
+
+
+
